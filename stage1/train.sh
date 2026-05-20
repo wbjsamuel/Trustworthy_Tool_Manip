7aadfa0f6c230ad48f14833bb69bbcc3ea70b115
wbjsamuel <wangbangjun@sjtu.edu.cn>
2026-05-20 15:14:23 +0000

    update

--- stage1/train.sh
+++ stage1/train.sh
@@ -4,9 +4,9 @@
 # Ensure you have installed the required dependencies from requirements.txt
 
 export HF_HOME="./cache"
-# export HF_TOKEN="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"
+
 export TORCH_HOME="./torch_cache"
-# export WANDB_API_KEY="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"
+
 
 echo "Starting Stage 1 training..."
 python3 -m stage1.train "$@"
